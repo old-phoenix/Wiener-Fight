ce405bd65e07483586ed7d80c1de1a5a64ead0a8
old-phoenix <99968439+old-phoenix@users.noreply.github.com>
2022-06-03 10:43:50 +0200

    Update app.ts

--- app.ts
+++ app.ts
@@ -10,8 +10,8 @@ writeFileSync(Utils.log_folder+'log.txt','');
 
 
 //generate new players
-let Player1 = new Player('Wiener Wurst');
-let Player2 = new Player('Wiener Schnitzel');
+let Player1: {name: string, strength: number, constitution: number, damage: number, health: number, max_health:number, hit_chance: number, crit_chance:number, block_chance: number,reflect_chance: number, skills: { id: number, name: string, type: number }[]} = new Player('Wiener Wurst');
+let Player2: {name: string, strength: number, constitution: number, damage: number, health: number, max_health:number, hit_chance: number, crit_chance:number, block_chance: number,reflect_chance: number, skills: { id: number, name: string, type: number }[]} = new Player('Wiener Schnitzel');
 
 //start fight
 const battle = new Fight(Player1, Player2);
